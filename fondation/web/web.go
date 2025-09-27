6fe3098400adc05e866e41182a103a33ec33fdd6
Admin <admin@MacBook-Pro.local>
2025-09-27 12:45:38 +0300

    add middleware support

--- fondation/web/web.go
+++ fondation/web/web.go
@@ -1,18 +1,41 @@
 package web
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"os"
 )
 
+type Handler func(context.Context, http.ResponseWriter, *http.Request) error
+
 type App struct {
 	*http.ServeMux
 	shutdonw chan os.Signal
+	mw       []MidHandler
 }
 
-func New(shutdonw chan os.Signal) *App {
+func New(shutdonw chan os.Signal, mw ...MidHandler) *App {
 	return &App{
 		ServeMux: http.NewServeMux(),
 		shutdonw: shutdonw,
+		mw:       mw,
 	}
+}
+
+func (a *App) HandleFunc(pattern string, handler Handler, mw ...MidHandler) {
+
+	handler = WrapMiddleware(mw, handler)
+	handler = WrapMiddleware(a.mw, handler)
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+		// any code here
+		if err := handler(r.Context(), w, r); err != nil {
+			fmt.Println(err)
+		}
+
+		// any code here
+	}
+	a.ServeMux.HandleFunc(pattern, h)
+
 }
