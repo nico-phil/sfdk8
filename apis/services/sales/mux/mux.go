e5ce4311125f13f9db7eaf8748b39324d42d5eac
Admin <admin@MacBook-Pro.local>
2025-09-27 15:12:03 +0300

    add loggin middleware

--- apis/services/sales/mux/mux.go
+++ apis/services/sales/mux/mux.go
@@ -4,11 +4,13 @@ import (
 	"os"
 
 	"github.com/nico-phil/service/apis/services/sales/route/sys/checkapi"
+	"github.com/nico-phil/service/app/api/mid"
+	"github.com/nico-phil/service/fondation/logger"
 	"github.com/nico-phil/service/fondation/web"
 )
 
-func WebAPI(shutdonw chan os.Signal) *web.App {
-	mux := web.New(shutdonw)
+func WebAPI(log *logger.Logger, shutdonw chan os.Signal) *web.App {
+	mux := web.New(shutdonw, mid.Logger(log))
 
 	checkapi.Routes(mux)
 
